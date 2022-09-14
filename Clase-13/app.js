dc6bfae5cca8f9d6871c5a80a3cc78e259cc0c75
CrisGadea <cristianhernangadea@gmail.com>
2022-09-14 20:19:22 -0300

    Clase 14: JS

---
+++ Clase-13/app.js
@@ -0,0 +1,41 @@
+// Hola soy un comentario de linea
+/*
+    Hola soy un comentario de bloque
+    Por eso puedo escribir
+    en muchos renglones
+*/
+// VARIABLES EN JS
+let nombre;
+let edad; // Variable no inicializada
+//var nombre = "Cristian"; // Variable con valor inicial Cristian
+//alert(nombre);
+//edad = 25;
+//nombre = "Hernán";
+//alert(nombre);
+
+
+// Mostrar informacion en el navegador (pagina web/HTML)
+
+// Mostramos un pop-up con la data
+//alert("WELCOME TO JAVASCRIPT!");
+// Mostramos la data por consola
+//console.log("Hola, mi nombre es " + nombre);
+//console.info("Soy una informacion desde la consola");
+//console.warn("ALERTA!");
+//console.error("ERROR!!");
+
+
+// Recibir informacion por teclado (a traves de un pop-up)
+//nombre = prompt("Escriba su nombre");
+edad = 25;
+
+const ANIO_ACTUAL = 2022;
+
+
+console.log(edad);
+//alert("Bienvenido/a " + nombre);
+console.log(nombre);
+
+// Mostrar el año actual
+alert("El año actual es: " + ANIO_ACTUAL);
+console.log(ANIO_ACTUAL);
